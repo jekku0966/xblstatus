a198056a4a0e8083743d312fcfcaa452a8254c23
jekku0966 <Jerva@Jerva-iMac.local>
2015-02-15 22:29:47 +0200

    Removed IF statements. No need for them.

--- status_local.py
+++ status_local.py
@@ -8,37 +8,37 @@ soup = BeautifulSoup(r.content)
 g_data = soup.find_all("ul", {"class": "core"})
 
 for item in g_data:
-	print 'TV, Music and Video ' + item.contents[1].find_all('span')[0].text
+	print 'TV, Music and Video is ' + item.contents[1].find_all('span')[0].text
 	try:
-		if item.contents[1].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-			print 'Affected platforms: ' + item.contents[1].find_all('p')[1].text
+		item.contents[1].find_all('span', {'class': 'unavailable'})[0].text
+		print 'Affected platforms: ' + item.contents[1].find_all('p')[1].text
 	except:
 		pass
-		print ''
-		print 'Xbox Live Core Services ' + item.contents[3].find_all('span')[0].text
+	print ''
+	print 'Xbox Live Core Services is ' + item.contents[3].find_all('span')[0].text
 	try:
-		if item.contents[3].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-			print 'Affected platforms: ' + item.contents[3].find_all('p')[1].text
+		item.contents[3].find_all('span', {'class': 'unavailable'})[0].text
+		print 'Affected platforms: ' + item.contents[3].find_all('p')[1].text
 	except:
 		pass
-		print ''
-		print 'Purchase and Content Usage ' + item.contents[5].find_all('span')[0].text
+	print ''
+	print 'Purchase and Content Usage is ' + item.contents[5].find_all('span')[0].text
 	try:
-			if item.contents[5].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-				print 'Affected platforms: ' + item.contents[5].find_all('p')[1].text
+		item.contents[5].find_all('span', {'class': 'unavailable'})[0].text
+		print 'Affected platforms: ' + item.contents[5].find_all('p')[1].text
 	except:
 		pass
-		print ''
-		print 'Website ' + item.contents[7].find_all('span')[0].text
+	print ''
+	print 'Website is ' + item.contents[7].find_all('span')[0].text
 	try:
-		if item.contents[7].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-			print 'Affected platforms: ' + item.contents[7].find_all('p')[1].text
+		item.contents[7].find_all('span', {'class': 'unavailable'})[0].text
+		print 'Affected platforms: ' + item.contents[7].find_all('p')[1].text
 	except:
 		pass
-		print ''
-		print 'Social and Gaming ' + item.contents[9].find_all('span')[0].text
+	print ''
+	print 'Social and Gaming is ' + item.contents[9].find_all('span')[0].text
 	try:
-		if item.contents[9].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-			print 'Affected platforms: ' + item.contents[9].find_all('p')[1].text
+		item.contents[9].find_all('span', {'class': 'unavailable'})[0].text
+		print 'Affected platforms: ' + item.contents[9].find_all('p')[1].text
 	except:
 		pass
