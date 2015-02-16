d9d205c7a91330bf0311473dca5db933018572a8
jekku0966 <Jerva@Jerva-iMac.local>
2015-02-17 01:05:06 +0200

    Trimmed code a bit.

--- status_local.py
+++ status_local.py
@@ -2,43 +2,80 @@ import requests
 from bs4 import BeautifulSoup
 
 r = requests.get('http://support.xbox.com/en-US/xbox-live-status?icid=furl_status')
-
 soup = BeautifulSoup(r.content)
-
 g_data = soup.find_all("ul", {"class": "core"})
+status = {}
+plat = {}
 
 for item in g_data:
-	print 'TV, Music and Video is ' + item.contents[1].find_all('span')[0].text
-	try:
-		item.contents[1].find_all('span', {'class': 'unavailable'})[0].text
-		print 'Affected platforms: ' + item.contents[1].find_all('p')[1].text+', '+item.contents[1].find_all('p')[2].text
-	except:
-		pass
-	print ''
-	print 'Xbox Live Core Services is ' + item.contents[3].find_all('span')[0].text
-	try:
-		item.contents[3].find_all('span', {'class': 'unavailable'})[0].text
-		print 'Affected platforms: ' + item.contents[3].find_all('p')[1].text+', '+item.contents[1].find_all('p')[2].text
-	except:
-		pass
-	print ''
-	print 'Purchase and Content Usage is ' + item.contents[5].find_all('span')[0].text
-	try:
-		item.contents[5].find_all('span', {'class': 'unavailable'})[0].text
-		print 'Affected platforms: ' + item.contents[5].find_all('p')[1].text+', '+item.contents[1].find_all('p')[2].text
-	except:
-		pass
-	print ''
-	print 'Website is ' + item.contents[7].find_all('span')[0].text
-	try:
-		item.contents[7].find_all('span', {'class': 'unavailable'})[0].text
-		print 'Affected platforms: ' + item.contents[7].find_all('p')[1].text+', '+item.contents[1].find_all('p')[2].text
-	except:
-		pass
-	print ''
-	print 'Social and Gaming is ' + item.contents[9].find_all('span')[0].text
-	try:
-		item.contents[9].find_all('span', {'class': 'unavailable'})[0].text
-		print 'Affected platforms: ' + item.contents[9].find_all('p')[1].text+', '+item.contents[1].find_all('p')[2].text
-	except:
-		pass
+        status['tmv']= item.contents[1].find_all('span')[0].text
+        try:
+                plat['tmv'] = item.contents[3].find_all('p')[1].text+', '+item.contents[3].find_all('p')[2].text
+        except:
+                pass
+        try:
+                plat['tmv'] = item.contents[1].find_all('p')[1].text
+        except:
+                pass
+        status['cores'] = item.contents[3].find_all('span')[0].text
+        try:
+                plat['cores'] = item.contents[3].find_all('p')[1].text+', '+item.contents[3].find_all('p')[2].text
+        except:
+                pass
+        try:
+                plat['cores'] = item.contents[3].find_all('p')[1].text
+        except:
+                pass
+        status['pc'] = item.contents[5].find_all('span')[0].text
+        try:
+                plat['pc'] = item.contents[5].find_all('p')[1].text+', '+item.contents[5].find_all('p')[2].text
+        except:
+                pass
+        try:
+                plat['pc'] = item.contents[5].find_all('p')[1].text
+        except:
+                pass
+        status['site'] = item.contents[7].find_all('span')[0].text
+        try:
+                plat['site'] = item.contents[7].find_all('p')[1].text+', '+item.contents[7].find_all('p')[2].text
+        except:
+                pass
+        try:
+                plat['site'] = item.contents[7].find_all('p')[1].text
+        except:
+                pass
+        status['sg'] = item.contents[9].find_all('span')[0].text
+        try:
+                plat['sg'] = item.contents[9].find_all('p')[1].text+', '+item.contents[9].find_all('p')[2].text
+        except:
+                pass
+        try:
+                plat['sg'] = item.contents[9].find_all('p')[1].text
+        except:
+                pass
+
+print 'TV, Music and Video is ' + status['tmv']
+try:
+	print 'Affected platforms: ' + plat['tmv']
+except:
+	pass
+print '\nXbox Live Core Services is ' + status['cores']
+try:
+	print 'Affected platforms: ' + plat['cores']
+except:
+	pass
+print '\nPurchase and Content Usage is ' + status['pc']
+try:
+	print 'Affected platforms: ' + plat['pc']
+except:
+	pass
+print '\nWebsite is ' + status['site']
+try:
+	print 'Affected platforms: ' + plat['site']
+except:
+	pass
+print '\nSocial and Gaming is ' + status['sg']
+try:
+	print 'Affected platforms: ' + plat['sg']
+except:
+	pass
