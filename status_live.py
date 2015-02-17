f719dbb440350a95d41a5b7966ac2ceef63dbb46
jekku0966 <Jerva@Jerva-iMac.local>
2015-02-18 01:47:33 +0200

    Still fixing code. Mistakes and solutions. At least this is a living code. Changes every day.

--- status_live.py
+++ status_live.py
@@ -8,7 +8,6 @@ class xblStatus(object):
 
 	def GET(self):
 		return json.dumps({'text': 'I\'m ALIVE!'})
-		print 'I\'m ALIVE!'
 
 	def POST(*args, **kwargs):
 		r = requests.get('http://support.xbox.com/en-US/xbox-live-status?icid=furl_status')
@@ -20,45 +19,54 @@ class xblStatus(object):
 		for item in g_data:
 			status['tmv'] = item.contents[1].find_all('span')[0].text
 			try:
-				plat['tmv'] = item.contents[3].find_all('p')[1].text+', '+item.contents[3].find_all('p')[2].text
+				plat['tmv'] = item.contents[1].find_all('p')[1].text+', '+item.contents[1].find_all('p')[2].text
 			except:
 				pass
-			try:
-				plat['tmv'] = item.contents[1].find_all('p')[1].text
-			except:
-				pass
+			if bool(plat) == False:
+				try:
+					plat['tmv'] = item.contents[1].find_all('p')[1].text
+				except:
+					pass
+
 			status['cores'] = item.contents[3].find_all('span')[0].text
 			try:
 				plat['cores'] = item.contents[3].find_all('p')[1].text+', '+item.contents[3].find_all('p')[2].text
 			except:
 				pass
-			try:
-				plat['cores'] = item.contents[3].find_all('p')[1].text
-			except:
-				pass
+			if bool(plat) == False:
+				try:
+					plat['cores'] = item.contents[3].find_all('p')[1].text
+				except:
+					pass
+
 			status['pc'] = item.contents[5].find_all('span')[0].text
 			try:
 				plat['pc'] = item.contents[5].find_all('p')[1].text+', '+item.contents[5].find_all('p')[2].text
 			except:
 				pass
-			try:
-				plat['pc'] = item.contents[5].find_all('p')[1].text
-			except:
-				pass
+			if bool(plat) == False:
+				try:
+					plat['pc'] = item.contents[5].find_all('p')[1].text
+				except:
+					pass
+
 			status['site'] = item.contents[7].find_all('span')[0].text
 			try:
 				plat['site'] = item.contents[7].find_all('p')[1].text+', '+item.contents[7].find_all('p')[2].text
 			except:
 				pass
-			try:
-				plat['site'] = item.contents[7].find_all('p')[1].text
-			except:
-				pass
+			if bool(plat) == False:
+				try:
+					plat['site'] = item.contents[7].find_all('p')[1].text
+				except:
+					pass
+
 			status['sg'] = item.contents[9].find_all('span')[0].text
 			try:
 				plat['sg'] = item.contents[9].find_all('p')[1].text+', '+item.contents[9].find_all('p')[2].text
 			except:
 				pass
+			if bool(plat) == False:
 				try:
 					plat['sg'] = item.contents[9].find_all('p')[1].text
 				except:
--- status_local.py
+++ status_local.py
@@ -8,51 +8,65 @@ status = {}
 plat = {}
 
 for item in g_data:
+
         status['tmv']= item.contents[1].find_all('span')[0].text
         try:
-                plat['tmv'] = item.contents[3].find_all('p')[1].text+', '+item.contents[3].find_all('p')[2].text
+                plat['tmv'] = item.contents[1].find_all('p')[1].text+', '+item.contents[1].find_all('p')[2].text
         except:
                 pass
-        try:
-                plat['tmv'] = item.contents[1].find_all('p')[1].text
-        except:
-                pass
+
+        if bool(plat) == False:
+                try:
+                        plat['tmv'] = item.contents[1].find_all('p')[1].text
+                except:
+                        pass
+
+
         status['cores'] = item.contents[3].find_all('span')[0].text
         try:
                 plat['cores'] = item.contents[3].find_all('p')[1].text+', '+item.contents[3].find_all('p')[2].text
         except:
                 pass
-        try:
-                plat['cores'] = item.contents[3].find_all('p')[1].text
-        except:
-                pass
+
+        if bool(plat) == False:
+                try:
+                        plat['cores'] = item.contents[3].find_all('p')[1].text
+                except:
+                        pass
+
         status['pc'] = item.contents[5].find_all('span')[0].text
         try:
                 plat['pc'] = item.contents[5].find_all('p')[1].text+', '+item.contents[5].find_all('p')[2].text
         except:
                 pass
-        try:
-                plat['pc'] = item.contents[5].find_all('p')[1].text
-        except:
-                pass
+        if bool(plat) == False:
+                try:
+                        plat['pc'] = item.contents[5].find_all('p')[1].text
+                except:
+                        pass
+
         status['site'] = item.contents[7].find_all('span')[0].text
         try:
                 plat['site'] = item.contents[7].find_all('p')[1].text+', '+item.contents[7].find_all('p')[2].text
         except:
                 pass
-        try:
-                plat['site'] = item.contents[7].find_all('p')[1].text
-        except:
-                pass
+        if bool(plat) == False:
+                try:
+                        plat['site'] = item.contents[7].find_all('p')[1].text
+                except:
+                        pass
+
         status['sg'] = item.contents[9].find_all('span')[0].text
         try:
                 plat['sg'] = item.contents[9].find_all('p')[1].text+', '+item.contents[9].find_all('p')[2].text
         except:
                 pass
-        try:
-                plat['sg'] = item.contents[9].find_all('p')[1].text
-        except:
-                pass
+        if bool(plat) == False:
+                try:
+                        plat['sg'] = item.contents[9].find_all('p')[1].text
+                except:
+                        pass
+
 
 print 'TV, Music and Video is ' + status['tmv']
 try:
