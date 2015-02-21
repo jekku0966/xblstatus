c316bb177bb636d2819313c42a112438b46d51ad
jekku0966 <Jerva@Jerva-iMac.local>
2015-02-21 22:37:04 +0200

    fixes upon fixes upon fixes...

--- status_live.py
+++ status_live.py
@@ -19,59 +19,57 @@ class xblStatus(object):
 			a = item.contents[1].find_all('h3')[0].text.split(item.contents[1].find_all('span')[0].text)[0] +' is ' + item.contents[1].find_all('span')[0].text
 			try:
 				if item.contents[1].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-					try:
+					if item.contents[1].find_all('p')[1].text == 'Xbox One' and item.contents[1].find_all('p')[2].text == 'Xbox 360':
 						a = a + '. ' + 'Platforms: ' + item.contents[1].find_all('p')[1].text + ', ' + item.contents[1].find_all('p')[2].text
-					except:
+					else:
 						a = a + '. ' +  'Platforms: ' + item.contents[1].find_all('p')[1].text
 			except:
 				pass
-				a = a
-
+			print a
 
 			b = '\n'+item.contents[3].find_all('h3')[0].text.split(item.contents[3].find_all('span')[0].text)[0] +' is ' + item.contents[3].find_all('span')[0].text
 			try:
 				if item.contents[3].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-					try:
-						b = b + '. ' +  'Platforms: ' + item.contents[3].find_all('p')[1].text + ', ' + item.contents[3].find_all('p')[2].text
-					except:
+					if item.contents[3].find_all('p')[1].text == 'Xbox One' and item.contents[3].find_all('p')[2].text == 'Xbox 360':
+						b = b + '. ' + 'Platforms: ' + item.contents[3].find_all('p')[1].text + ', ' + item.contents[3].find_all('p')[2].text
+					else:
 						b = b + '. ' +  'Platforms: ' + item.contents[3].find_all('p')[1].text
 			except:
 				pass
-				b = b
+			print b
 
 			c = '\n'+item.contents[5].find_all('h3')[0].text.split(item.contents[5].find_all('span')[0].text)[0] +' is ' + item.contents[5].find_all('span')[0].text
 			try:
 				if item.contents[5].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-					try:
-						c = c + '. ' +  'Platforms: ' + item.contents[5].find_all('p')[1].text + ', ' + item.contents[5].find_all('p')[2].text
-					except:
+					if item.contents[5].find_all('p')[1].text == 'Xbox One' and item.contents[5].find_all('p')[2].text == 'Xbox 360':
+						c = c + '. ' + 'Platforms: ' + item.contents[5].find_all('p')[1].text + ', ' + item.contents[5].find_all('p')[2].text
+					else:
 						c = c + '. ' +  'Platforms: ' + item.contents[5].find_all('p')[1].text
 			except:
 				pass
-				sc = c
+			print c
 
 			d = '\n'+item.contents[7].find_all('h3')[0].text.split(item.contents[7].find_all('span')[0].text)[0] +' is ' + item.contents[7].find_all('span')[0].text
 			try:
 				if item.contents[7].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-					try:
-						d = d + '. ' +  'Platforms: ' + item.contents[7].find_all('p')[1].text + ', ' + item.contents[7].find_all('p')[2].text
-					except:
+					if item.contents[7].find_all('p')[1].text == 'Xbox One' and item.contents[7].find_all('p')[2].text == 'Xbox 360':
+						d = d + '. ' + 'Platforms: ' + item.contents[7].find_all('p')[1].text + ', ' + item.contents[7].find_all('p')[2].text
+					else:
 						d = d + '. ' +  'Platforms: ' + item.contents[7].find_all('p')[1].text
 			except:
 				pass
-				d = d
+			print d
 
 			e = '\n'+item.contents[9].find_all('h3')[0].text.split(item.contents[9].find_all('span')[0].text)[0] +' is ' + item.contents[9].find_all('span')[0].text
 			try:
 				if item.contents[9].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-					try:
-						e = e + '. ' +  'Platforms: ' + item.contents[9].find_all('p')[1].text + ', ' + item.contents[9].find_all('p')[2].text
-					except:
+					if item.contents[9].find_all('p')[1].text == 'Xbox One' and item.contents[9].find_all('p')[2].text == 'Xbox 360':
+						e = e + '. ' + 'Platforms: ' + item.contents[9].find_all('p')[1].text + ', ' + item.contents[9].find_all('p')[2].text
+					else:
 						e = e + '. ' +  'Platforms: ' + item.contents[9].find_all('p')[1].text
-						pass
 			except:
 				pass
-				e = e
+			print e
 
 			return json.dumps({'text': '%s%s%s%s%s' % (a,b,c,d,e)})
 
--- status_local.py
+++ status_local.py
@@ -10,67 +10,54 @@ for item in g_data:
 	a = item.contents[1].find_all('h3')[0].text.split(item.contents[1].find_all('span')[0].text)[0] +' is ' + item.contents[1].find_all('span')[0].text
 	try:
 		if item.contents[1].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-			try:
+			if item.contents[1].find_all('p')[1].text == 'Xbox One' and item.contents[1].find_all('p')[2].text == 'Xbox 360':
 				a = a + '. ' + 'Platforms: ' + item.contents[1].find_all('p')[1].text + ', ' + item.contents[1].find_all('p')[2].text
-				print a
-			except:
+			else:
 				a = a + '. ' +  'Platforms: ' + item.contents[1].find_all('p')[1].text
-				print a
 	except:
 		pass
-		print a
+	print a
 
 	b = '\n'+item.contents[3].find_all('h3')[0].text.split(item.contents[3].find_all('span')[0].text)[0] +' is ' + item.contents[3].find_all('span')[0].text
 	try:
 		if item.contents[3].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-			try:
-				b = b + '. ' +  'Platforms: ' + item.contents[3].find_all('p')[1].text + ', ' + item.contents[3].find_all('p')[2].text
-				print b
-			except:
+			if item.contents[3].find_all('p')[1].text == 'Xbox One' and item.contents[3].find_all('p')[2].text == 'Xbox 360':
+				b = b + '. ' + 'Platforms: ' + item.contents[3].find_all('p')[1].text + ', ' + item.contents[3].find_all('p')[2].text
+			else:
 				b = b + '. ' +  'Platforms: ' + item.contents[3].find_all('p')[1].text
-				print b
-				pass
 	except:
 		pass
-		print b
+	print b
 
 	c = '\n'+item.contents[5].find_all('h3')[0].text.split(item.contents[5].find_all('span')[0].text)[0] +' is ' + item.contents[5].find_all('span')[0].text
 	try:
 		if item.contents[5].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-			try:
-				c = c + '. ' +  'Platforms: ' + item.contents[5].find_all('p')[1].text + ', ' + item.contents[5].find_all('p')[2].text
-				print c
-			except:
+			if item.contents[5].find_all('p')[1].text == 'Xbox One' and item.contents[5].find_all('p')[2].text == 'Xbox 360':
+				c = c + '. ' + 'Platforms: ' + item.contents[5].find_all('p')[1].text + ', ' + item.contents[5].find_all('p')[2].text
+			else:
 				c = c + '. ' +  'Platforms: ' + item.contents[5].find_all('p')[1].text
-				print c
 	except:
 		pass
-		print c
+	print c
 
 	d = '\n'+item.contents[7].find_all('h3')[0].text.split(item.contents[7].find_all('span')[0].text)[0] +' is ' + item.contents[7].find_all('span')[0].text
 	try:
 		if item.contents[7].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-			try:
-				d = d + '. ' +  'Platforms: ' + item.contents[7].find_all('p')[1].text + ', ' + item.contents[7].find_all('p')[2].text
-				print d
-			except:
+			if item.contents[7].find_all('p')[1].text == 'Xbox One' and item.contents[7].find_all('p')[2].text == 'Xbox 360':
+				d = d + '. ' + 'Platforms: ' + item.contents[7].find_all('p')[1].text + ', ' + item.contents[7].find_all('p')[2].text
+			else:
 				d = d + '. ' +  'Platforms: ' + item.contents[7].find_all('p')[1].text
-				print d
-				pass
 	except:
 		pass
-		print d
+	print d
 
 	e = '\n'+item.contents[9].find_all('h3')[0].text.split(item.contents[9].find_all('span')[0].text)[0] +' is ' + item.contents[9].find_all('span')[0].text
 	try:
 		if item.contents[9].find_all('span', {'class': 'unavailable'})[0].text == 'Limited':
-			try:
-				e = e + '. ' +  'Platforms: ' + item.contents[9].find_all('p')[1].text + ', ' + item.contents[9].find_all('p')[2].text
-				print e
-			except:
+			if item.contents[9].find_all('p')[1].text == 'Xbox One' and item.contents[9].find_all('p')[2].text == 'Xbox 360':
+				e = e + '. ' + 'Platforms: ' + item.contents[9].find_all('p')[1].text + ', ' + item.contents[9].find_all('p')[2].text
+			else:
 				e = e + '. ' +  'Platforms: ' + item.contents[9].find_all('p')[1].text
-				print e
-				pass
 	except:
 		pass
-		print e
+	print e
