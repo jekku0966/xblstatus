97b0f77fb4bc611d6689d0e6d126470031aeb957
jekku0966 <Jerva@Jerva-iMac.local>
2016-08-22 22:15:19 +0300

    Latest and greatest. Already forgotten what I've changed but nothing is broken and this was an uncommitted change in SourceTree. It still works so no panic.

--- status_live.py
+++ status_live.py
@@ -3,42 +3,45 @@ from bs4 import BeautifulSoup
 import cherrypy
 import json
 
+
 class xblStatus(object):
-	exposed = True
+    exposed = True
 
-	def GET(self):
-		return json.dumps({'text': 'I\'m ALIVE!'})
+    def GET(self):
+        return json.dumps({'text': 'I\'m ALIVE!'})
 
-	def POST(*args, **kwargs):
-		r = requests.get('http://support.xbox.com/en-US/xbox-live-status?icid=furl_status')
-		soup = BeautifulSoup(r.content, 'html5lib')
-		g_data = soup.find_all("ul", {"class": "core"})
-		status = {}
+    def POST(*args, **kwargs):
+        r = requests.get(
+            'http://support.xbox.com/en-US/xbox-live-status?icid=furl_status')
+        soup = BeautifulSoup(r.content, 'html5lib')
+        g_data = soup.find_all("ul", {"class": "core"})
+        status = {}
 
-		for item in g_data:
-			for service in item.findAll('h3', {'class': 'servicename'}):
-				status[service.text] = []
-			for platform in item.findAll('div', {'class': 'label'}):
-				status[service.text].append(platform.text)
-		outputOk = []
-		outputDown = []
-		for service,platform in status.items():
-			if platform:
-				platString = ''.join(platform)
-				outputDown.append('{} is limited. Platforms: {}\n'.format(service, platString))
-			else:
-				outputOk.append('{} is up and running.\n'.format(service))
-			strOk = ''.join(outputOk)
-			strDown = ''.join(outputDown)
-		if strDown and strOk:
-			return json.dumps({'attachments': [{'fallback': strOk, 'title': 'These systems are online:', 'text': strOk, 'color': '#008000'}, {'fallback': strDown, 'title': 'These systems are down:', 'text': strDown, 'color': '#FF0000'}]})
-		elif strDown and not strOk:
-			return json.dumps({'attachments': [	{'fallback': strDown, 'title': 'These systems are down:', 'text': strDown, 'color': '#FF0000'}]})
-		else:
-			return json.dumps({'attachments': [{'fallback': strOk, 'title': 'These systems are online:', 'text': strOk, 'color': '#008000'}]})
+        for item in g_data:
+            for service in item.findAll('h3', {'class': 'servicename'}):
+                status[service.text] = []
+            for platform in item.findAll('div', {'class': 'label'}):
+                status[service.text].append(platform.text)
+        outputOk = []
+        outputDown = []
+        for service, platform in status.items():
+            if platform:
+                platString = ''.join(platform)
+                outputDown.append(
+                    '{} is limited. Platforms: {}\n'.format(service, platString))
+            else:
+                outputOk.append('{} is up and running.\n'.format(service))
+            strOk = ''.join(outputOk)
+            strDown = ''.join(outputDown)
+        if strDown and strOk:
+            return json.dumps({'attachments': [{'fallback': strOk, 'title': 'These systems are online:', 'text': strOk, 'color': '#008000'}, {'fallback': strDown, 'title': 'These systems are down:', 'text': strDown, 'color': '#FF0000'}]})
+        elif strDown and not strOk:
+            return json.dumps({'attachments': [	{'fallback': strDown, 'title': 'These systems are down:', 'text': strDown, 'color': '#FF0000'}]})
+        else:
+            return json.dumps({'attachments': [{'fallback': strOk, 'title': 'These systems are online:', 'text': strOk, 'color': '#008000'}]})
 
 
-# Start the Cherrypy server        
+# Start the Cherrypy server
 if __name__ == '__main__':
     cherrypy.config.update("server.conf")
     cherrypy.quickstart(xblStatus(), '/', "app.conf")
--- status_local.py
+++ status_local.py
@@ -6,7 +6,8 @@ soup = BeautifulSoup(r.content, 'html5lib')
 g_data = soup.findAll('ul', {'class': 'core'})
 
 status = {}
-# For testing purposes add these to status dictionary: 'lol': ['x1, 360, none'], 'test': ['teet, 111, XxX']
+# For testing purposes add these to status dictionary: 'lol': ['x1, 360,
+# none'], 'test': ['teet, 111, XxX']
 
 
 for item in g_data:
@@ -15,8 +16,8 @@ for item in g_data:
         status[service.text] = []
     for platform in item.findAll('div', {'class': 'label'}):
         status[service.text].append(platform.text)
-    
-for service,platform in status.items():
+
+for service, platform in status.items():
     if platform:
         platString = ''.join(platform)
         print('{} is limited. Platforms: {}'.format(service, platString))
